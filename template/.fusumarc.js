eef69908d4c09e3f2d9bb4cd5230b092f4740a1f
yumekiti <yumekiti120440@gmail.com>
2022-07-28 01:10:30 +0900

    `chenge` template

--- template/.fusumarc.js
+++ template/.fusumarc.js
@@ -5,17 +5,17 @@ const basePath = 'hoge';
 module.exports = {
   meta: {
     ...base.meta,
-    title: '',
-    description: '',
-    url: `${base.meta.url}${basePath}`
+    title: 'hoge',
+    description: 'this is hoge',
+    url: `${base.meta.url}${basePath}`,
   },
   slide: {
     showIndex: true,
     code: {
-      languages: ['json', 'markdown', 'javascript', 'css']
-    }
+      languages: ['json', 'markdown', 'javascript', 'css'],
+    },
   },
   extends: {
-    css: 'style.css'
+    css: 'style.css',
   },
 };
